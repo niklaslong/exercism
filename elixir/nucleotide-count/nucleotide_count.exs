9e8fc16c5692b6b7896e2c7afb437bd30cc67b62
niklaslong <niklas@long.ch>
2017-10-08 15:13:31 +0200

    solve nucleotide_count

--- elixir/nucleotide-count/nucleotide_count.exs
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -14,7 +14,8 @@ defmodule NucleotideCount do
   """
   @spec count([char], char) :: non_neg_integer
   def count(strand, nucleotide) do
-
+    strand 
+    |> Enum.count(fn x -> x == nucleotide end)
   end
 
 
@@ -28,6 +29,6 @@ defmodule NucleotideCount do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
-
+    %{?A => count(strand, ?A), ?T => count(strand, ?T), ?C => count(strand, ?C), ?G => count(strand, ?G)}
   end
 end
