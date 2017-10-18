2b0a06a8ea02ddae7049555dba16fe0b9e8f0e17
niklaslong <niklas@long.ch>
2017-10-18 12:58:59 +0200

    add solution for secret handshake

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,4 +1,5 @@
 defmodule SecretHandshake do
+use Bitwise
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -13,8 +14,22 @@ defmodule SecretHandshake do
 
   10000 = Reverse the order of the operations in the secret handshake
   """
+
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    []
+    |> append_if_true(code, 1, ["wink"])
+    |> append_if_true(code, 2, ["double blink"])
+    |> append_if_true(code, 4, ["close your eyes"])
+    |> append_if_true(code, 8, ["jump"])
+    |> reverse_if_true(code, 16)
+  end
+
+  defp append_if_true(list, code, n, str) do
+    if (code &&& n) === n, do: list ++ str, else: list
+  end
+
+  defp reverse_if_true(list, code, n) do
+    if (code &&& n) === n, do: list |> Enum.reverse(), else: list
   end
 end
-
