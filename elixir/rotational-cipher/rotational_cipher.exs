59bd20246c4f8d9e0c8aa678d29523a80ae251fd
niklaslong <niklas@long.ch>
2017-10-18 15:08:11 +0200

    add solution for rotational cypher

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -8,6 +8,20 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    text 
+    |> String.split("", trim: true) #returns list of 
+    |> Enum.map(&add_shift(&1, shift))
+    |> Enum.join("")
+  end
+
+  defp add_shift(char, shift) do
+    <<code, _>> = char <> <<0>>
+    cond do
+      code in 65..90 && code + shift < 91 -> <<code + shift>>
+      code in 65..90 && code + shift > 90 -> <<code + shift - 26>>
+      code in 97..122 && code + shift < 123 -> <<code + shift>>
+      code in 97..122 && code + shift > 122 -> <<code + shift - 26>>
+      true -> <<code>>
+    end
   end
 end
-
