e856b54e78d7077164fe6eca69405b0dafbec856
niklaslong <niklas@long.ch>
2017-11-24 18:39:27 +0100

    implement full solution

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -3,8 +3,19 @@ defmodule Bob do
     cond do
         String.split(input, ~r{\s}, trim: true) == [] -> "Fine. Be that way!"
         String.last(input) == "?" -> "Sure."
+        Regex.match?(~r/\p{L}/, input) == false -> "Whatever."
         input == String.upcase(input) -> "Whoa, chill out!"
         true -> "Whatever."
     end
   end
+
+  # def contains_nums(input) do
+  #   input
+  #   |> String.split(",")
+  #   |> Enum.join("")
+
+  #   case Regex.match?(~r/[a-zA-Z]/) do
+  #     true
+  #   end
+  # end
 end
