f029e6ec14ecc9f5accd2b30ba50da3bc62eb0b9
niklaslong <niklas@long.ch>
2017-11-24 18:43:36 +0100

    refactor

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -3,8 +3,7 @@ defmodule Bob do
     cond do
         String.split(input, ~r{\s}, trim: true) == [] -> "Fine. Be that way!"
         String.last(input) == "?" -> "Sure."
-        Regex.match?(~r/\p{L}/, input) == false -> "Whatever."
-        input == String.upcase(input) -> "Whoa, chill out!"
+        input == String.upcase(input) and Regex.match?(~r/\p{L}/, input) -> "Whoa, chill out!"
         true -> "Whatever."
     end
   end
