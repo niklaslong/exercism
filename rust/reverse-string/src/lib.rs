ee52dc1a9aeeadff71ceb78e8d1a09c281decbb9
niklaslong <niklas@long.ch>
2018-06-08 21:51:05 +0200

    add grapheme cluster solution

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,11 @@
+extern crate unicode_segmentation;
+
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    // using chars (won't work for uüu)
+    // input.chars().rev().collect()
+
+    // using grapheme clusters:
+    input.graphemes(true).rev().collect()
 }
